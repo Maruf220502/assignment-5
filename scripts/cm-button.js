4a9453268fea5f577a3644d072d7fdb9bf1220c5
Maruf220502 <khan2002maruf@gmail.com>
2025-03-01 22:25:43 +0600

    some javaScript part

---
+++ scripts/cm-button.js
@@ -0,0 +1,139 @@
+document.getElementById("btn-1")
+.addEventListener("click", function(event){
+    event.preventDefault();
+
+    document.getElementById("btn-1").setAttribute("disabled", "true");
+    const btn1 = getInnerTextByID("task-number");
+    const sum = btn1-1;
+    const navNumber = getInnerTextByID("nav-number");
+    const add = navNumber + 1;
+
+    if(sum>=0){
+        alert("Board Updated Sucessfully")
+        if(sum=== 0){
+            alert("congrates!!!you have completed all the task")
+        }
+    }
+    
+    setInnerTextByIDandValue("task-number",sum );
+    setInnerTextByIDandValue("nav-number",add );
+})
+
+// button-2
+
+document.getElementById("btn-2")
+.addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("btn-2").setAttribute("disabled", "true");
+
+    const btn1 = getInnerTextByID("task-number");
+    const sum = btn1-1;
+    const navNumber = getInnerTextByID("nav-number");
+    const add = navNumber + 1;
+
+    if(sum>=0){
+        alert("Board Updated Sucessfully")
+        if(sum=== 0){
+            alert("congrates!!!you have completed all the task")
+        }
+    }
+    
+    setInnerTextByIDandValue("task-number",sum );
+    setInnerTextByIDandValue("nav-number",add );
+})
+
+// button-3
+
+document.getElementById("btn-3")
+.addEventListener("click", function(event){
+    event.preventDefault();
+
+    document.getElementById("btn-3").setAttribute("disabled", "true");
+
+    const btn1 = getInnerTextByID("task-number");
+    const sum = btn1-1;
+    const navNumber = getInnerTextByID("nav-number");
+    const add = navNumber + 1;
+
+    if(sum>=0){
+        alert("Board Updated Sucessfully")
+        if(sum=== 0){
+            alert("congrates!!!you have completed all the task")
+        }
+    }
+    
+    setInnerTextByIDandValue("task-number",sum );
+    setInnerTextByIDandValue("nav-number",add );
+})
+
+// button-4
+
+document.getElementById("btn-4")
+.addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("btn-4").setAttribute("disabled", "true");
+
+    const btn1 = getInnerTextByID("task-number");
+    const sum = btn1-1;
+    const navNumber = getInnerTextByID("nav-number");
+    const add = navNumber + 1;
+
+    if(sum>=0){
+        alert("Board Updated Sucessfully")
+        if(sum=== 0){
+            alert("congrates!!!you have completed all the task")
+        }
+    }
+    
+    setInnerTextByIDandValue("task-number",sum );
+    setInnerTextByIDandValue("nav-number",add );
+})
+
+// button-5
+
+document.getElementById("btn-5")
+.addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("btn-5").setAttribute("disabled", "true");
+
+    const btn1 = getInnerTextByID("task-number");
+    const sum = btn1-1;
+    const navNumber = getInnerTextByID("nav-number");
+    const add = navNumber + 1;
+
+    if(sum>=0){
+        alert("Board Updated Sucessfully")
+        if(sum=== 0){
+            alert("congrates!!!you have completed all the task")
+        }
+    }
+    
+    setInnerTextByIDandValue("task-number",sum );
+    setInnerTextByIDandValue("nav-number",add );
+})
+
+// button-6
+
+document.getElementById("btn-6")
+.addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("btn-6").setAttribute("disabled", "true");
+
+    const btn1 = getInnerTextByID("task-number");
+    const sum = btn1-1;
+    const navNumber = getInnerTextByID("nav-number");
+    const add = navNumber + 1;
+
+    if(sum>=0){
+        alert("Board Updated Sucessfully")
+        if(sum=== 0){
+            alert("congrates!!!you have completed all the task")
+        }
+    }
+    
+    setInnerTextByIDandValue("task-number",sum );
+    setInnerTextByIDandValue("nav-number",add );
+})
+
+
+
