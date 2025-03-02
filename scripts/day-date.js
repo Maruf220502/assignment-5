a322c15ec77a11066b1f774196bb9c7253ce34da
Maruf220502 <khan2002maruf@gmail.com>
2025-03-02 08:20:37 +0600

    more javaScript

--- scripts/day-date.js
+++ scripts/day-date.js
@@ -2,11 +2,11 @@ function updateDate() {
     const date = new Date();
     
    
-    const options = { weekday: 'short', month: 'short', day: '2-digit', year: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-US', options).replace(',', '');
+    const changeDay = { weekday: 'short', month: 'short', day: '2-digit', year: 'numeric' };
+    const theNewDate = date.toLocaleDateString('en-US', changeDay).replace(',', '');
     
     
-    const [day, month, dayNum, year] = formattedDate.split(' ');
+    const [day, month, dayNum, year] = theNewDate.split(' ');
 
     document.getElementById("dayText").innerText = day; 
     document.getElementById("dateText").innerText = 
